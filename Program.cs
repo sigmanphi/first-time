be89d8b86377f5f6b314a1f5167bb61e5a984d29
Joel Welden-Smith <joelweldensmith@gmail.com>
2019-08-21 19:35:03 -0700

    first version

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace JoelApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string n;
+            string a;
+
+            Console.WriteLine("Please enter name:");
+            n = Console.ReadLine();
+            Console.WriteLine("Please enter address:");
+            a = Console.ReadLine();
+            Person joel = new Person(n, a);
+            Console.WriteLine(joel.Name + " is my name");
+        }
+    }
+
+    public class Bankaccount
+    {
+        public decimal Balance { get; }
+        public string Owner { get; set; }
+        public string AccountNumber { get; }
+
+        private static int accountstartnum = 1234567890;
+
+        public Bankaccount(string name, decimal initialbalance)
+        {
+            Owner = name;
+            Balance = initialbalance;
+        }
+
+        public void MakeWithdrawal (int amount, string note, DateTime date)
+        {
+
+        }
+
+        public void MakeDeposite(int amount, string note, DateTime date)
+        {
+
+        }
+
+    }
+
+}
